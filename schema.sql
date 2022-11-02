4bc3309792571fec77cbc648afb8ae2584bb5e01
Marco Oquendo <marcooquendoc@gmail.com>
2022-11-02 01:21:57 -0500

    Create a table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
+/*
+  original code
+  CREATE TABLE animals (
+      name varchar(100)
+  );
+*/
 
 CREATE TABLE animals (
-    name varchar(100)
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  date_of_birth DATE,
+  escape_attempts INT NOT NULL CHECK (escape_attempts >= 0),
+  neutered BOOL,
+  weight_kg FLOAT(2)
 );
