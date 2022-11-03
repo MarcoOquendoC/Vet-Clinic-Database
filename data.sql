01e6e19d94cf3f9d2fe9234a87816be05ad6256f
Marco Oquendo <marcooquendoc@gmail.com>
2022-11-03 03:12:18 -0500

    Wednesday tasks

--- data.sql
+++ data.sql
@@ -14,7 +14,7 @@
     ('Agumon', DATE '2020-02-03', 10.23, true, 0),
     ('Gabumon', DATE '2018-11-15', 8.00, true, 2),
     ('Pikachu', DATE '2021-01-07', 15.04, false, 1),
-    ('Debimon', DATE '2017-05-12', 11.00, true, 5);
+    ('Devimon', DATE '2017-05-12', 11.00, true, 5);
 */
 
 -- Tuesday
@@ -37,3 +37,26 @@ VALUES
 */
 
 -- Wednesday
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith' ) WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
+
